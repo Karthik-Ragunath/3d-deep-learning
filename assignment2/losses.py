419f08e4434192f0a2168e5fa4d69f90db02e50e
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-09-21 01:40:50 -0500

    added code for modifying loss function to fit voxel

--- assignment2/losses.py
+++ assignment2/losses.py
@@ -4,8 +4,20 @@ import torch
 def voxel_loss(voxel_src,voxel_tgt):
 	# voxel_src: b x h x w x d
 	# voxel_tgt: b x h x w x d
+
+	# loss = torch.nn.BCELoss() # wont work because of bounds issue
+	
+	# relu_activation = torch.nn.ReLU()
+	# loss = torch.nn.BCELoss()
+	# output = loss(relu_activation(voxel_src), voxel_tgt)
+
+	# loss = torch.nn.BCEWithLogitsLoss() # works
+	# output = loss(voxel_src, voxel_tgt)
+
+	sigmoid_activation = torch.nn.Sigmoid()
 	loss = torch.nn.BCELoss()
-	output = loss(voxel_src, voxel_tgt)
+	output = loss(sigmoid_activation(voxel_src), voxel_tgt)
+
 	# implement some loss for binary voxel grids
 	return output
 
