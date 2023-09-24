8a39bf2449bbd4d3b4c9e6cb41f95f5b40e6c5f5
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2023-09-24 05:27:10 -0500

    fixed issues

--- assignment2/train_model.py
+++ assignment2/train_model.py
@@ -24,7 +24,7 @@ def get_args_parser():
     parser.add_argument('--n_points', default=5000, type=int)
     parser.add_argument('--w_chamfer', default=1.0, type=float)
     parser.add_argument('--w_smooth', default=0.1, type=float)
-    parser.add_argument('--save_freq', default=10000, type=int)    
+    parser.add_argument('--save_freq', default=5000, type=int)
     parser.add_argument('--device', default='cuda', type=str) 
     parser.add_argument('--load_feat', action='store_true') 
     parser.add_argument('--load_checkpoint', action='store_true')            
